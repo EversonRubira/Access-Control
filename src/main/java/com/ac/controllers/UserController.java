2f5cc4ca99fd54d825a6161449d4d11e925e6034
Everson_Rubira <85372823+EversonRubira@users.noreply.github.com>
2022-10-20 12:07:22 +0100

    Update UserController.java

--- src/main/java/com/ac/controllers/UserController.java
+++ src/main/java/com/ac/controllers/UserController.java
@@ -7,13 +7,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController    {
     
-    @GetMapping("/users/v1/")
+    @PostMapping("/users/v1/")
     public String create(Object o) {
         return "Creating user...";             
     }
-
     
-
+    @GetMapping("/users/v1/")
+    @ResponseBody
+    public String read(Object o) {
+        return "Sharing user...";             
+    }
+    
+    @PutMapping("/users/v1/")
+    @ResponseBody
+    public String edit(Object o) {
+        return "Updating...";             
+    }
+    
+    @DelMapping("/users/v1/")
+    @ResponseBody
+    public String delete(Object o) {
+        return "Deleting user...";             
+    }
+    
+    
     //localhost:8080/pong/Everson
 }
-git add

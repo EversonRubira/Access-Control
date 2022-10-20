6aa0fcd27805a03288fee98e8952db47eed5b2fa
EversonRubira <eversonrubira@gmail.com>
2022-10-20 22:11:53 +0100

    Access-Control

--- src/main/java/com/ac/controllers/UserController.java
+++ src/main/java/com/ac/controllers/UserController.java
@@ -1,19 +1,37 @@
 package com.ac.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController    {
     
-    @GetMapping("/users/v1/")
+    
+
+    @PostMapping("/USERS_V1/")
     public String create(Object o) {
         return "Creating user...";             
     }
-
+   
+      @RequestMapping("/USERS_V2/")
+    public String read(Object o) {
+        return "Sharing user...";             
+    }
     
+     @PutMapping("/users/v3/")
+    public String edit(Object o) {
+        return "Updating...";             
+    }
+    
+    @DeleteMapping("/users/v4/")
+    public String delete(Object o) {
+        return "Deleting user...";             
+    }
+
+        
 
     //localhost:8080/pong/Everson
 }
-git add

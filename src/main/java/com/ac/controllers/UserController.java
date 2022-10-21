8e765d70588ec54f8a430df6cbab5d3c0e3cf034
Everson_Rubira <85372823+EversonRubira@users.noreply.github.com>
2022-10-21 11:48:32 +0100

    Update UserController.java

--- src/main/java/com/ac/controllers/UserController.java
+++ src/main/java/com/ac/controllers/UserController.java
@@ -5,27 +5,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class UserController    {
     
     @PostMapping("/users/v1/")
+    //@RequestMapping(value = /users/v1, method = RequestMethod.POST) 
     public String create(Object o) {
         return "Creating user...";             
     }
     
     @GetMapping("/users/v2/")
-    @ResponseBody
+    //@RequestMapping(value = /users/v1, method = RequestMethod.GET)
     public String read(Object o) {
         return "Sharing user...";             
     }
     
     @PutMapping("/users/v3/")
-    @ResponseBody
+    //@RequestMapping(value = /users/v1, method = RequestMethod.PUT)
     public String edit(Object o) {
         return "Updating...";             
     }
     
     @DelMapping("/users/v4/")
-    @ResponseBody
+    //@RequestMapping(value = /users/v1, method = RequestMethod.DELETE)
     public String delete(Object o) {
         return "Deleting user...";             
     }

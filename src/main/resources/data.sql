8d5d1cdf47c94c4c2e3380b69afb30cb1067a475
EversonRubira <eversonrubira@gmail.com>
2022-10-24 17:39:06 +0100

    update DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO USER(id, name, age, email, profession) VALUES('123', 'Everson', '49', 'eversonrubira@gmail.com', 'desenvolvedor java');
-INSERT INTO USER(id, name, age, email, profession) VALUES('233', 'Cadu', '40', 'cadu@gmail.com', 'desenvolvedor java');
-INSERT INTO USER(id, name, age, email, profession) VALUES('555', 'Arthur', '07', 'arthur@gmail.com', 'judoca');
-INSERT INTO USER(id, name, age, email, profession) VALUES('123', 'Ben', '08', 'benserpa@gmail.com', 'jogador futebol');
+INSERT INTO USER(id, name, age, email, profession) VALUES('124', 'Cadu', '40', 'cadu@gmail.com', 'desenvolvedor java');
+INSERT INTO USER(id, name, age, email, profession) VALUES('125', 'Arthur', '07', 'arthur@gmail.com', 'judoca');
+INSERT INTO USER(id, name, age, email, profession) VALUES('126', 'Ben', '08', 'benserpa@gmail.com', 'jogador futebol');
